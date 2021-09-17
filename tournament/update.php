43c1cc66083ac9c4dfd8e13b97bbb9111f81a8fe
cindytsai <6188366+cindytsai@users.noreply.github.com>
2021-09-17 17:11:57 +0800

    Distinguish "0"->Ask For Leave, "-1"->Abstainer, and ""->null.

--- tournament/update.php
+++ tournament/update.php
@@ -22,7 +22,27 @@ $gap = 3 * ($distribute['3_1'] + $distribute['3_2']) + 6 * ($distribute['4_1'] +
 for ($i = 1; $i < count($above); $i++) {
 	$temp_above = $above[$i];
 	$temp_below = $below[$i];
-	if ($temp_above > $temp_below) {
+	
+	// "" -> -10 in ./resource/custom.js
+	// If $temp_above = $temp_below = -10 , reset the score as NULL
+	if ($temp_above == $temp_below && $temp_above == -10){
+		mysqli_query($mysql, "UPDATE GAMESTATE SET ABOVESCORE=NULL, BELOWSCORE=NULL, WINNER=NULL WHERE USERNO='$account' AND GAMENO='$gameno' AND PLAYNO='$i'");
+	}
+	// If both of them are "Abstainer" -> -1
+	elseif ($temp_above == $temp_below && $temp_above == -1) {
+		mysqli_query($mysql, "UPDATE GAMESTATE SET ABOVESCORE='$temp_above', BELOWSCORE='$temp_below', WINNER='-1' WHERE USERNO='$account' AND GAMENO='$gameno' AND PLAYNO='$i'");
+	}
+	// If both of them "Ask For Leave" -> 0
+	elseif ($temp_above == $temp_below && $temp_above == 0) {
+		srand($gap + $amount + 10 * $i);
+		if (rand() % 2 == 0){
+			mysqli_query($mysql, "UPDATE GAMESTATE SET ABOVESCORE=0, BELOWSCORE=0, WINNER=ABOVE WHERE USERNO='$account' AND GAMENO='$gameno' AND PLAYNO='$i'");
+		}
+		else {
+			mysqli_query($mysql, "UPDATE GAMESTATE SET ABOVESCORE=0, BELOWSCORE=0, WINNER=BELOW WHERE USERNO='$account' AND GAMENO='$gameno' AND PLAYNO='$i'");
+		}
+	}
+	elseif ($temp_above > $temp_below) {
 		mysqli_query($mysql, "UPDATE GAMESTATE SET ABOVESCORE='$temp_above', BELOWSCORE='$temp_below', WINNER=ABOVE WHERE USERNO='$account' AND GAMENO='$gameno' AND PLAYNO='$i'");
 	}
 	elseif ($temp_above < $temp_below) {
@@ -31,6 +51,7 @@ for ($i = 1; $i < count($above); $i++) {
 	elseif ($temp_above == $temp_below && $temp_above != '') {
 		mysqli_query($mysql, "UPDATE GAMESTATE SET ABOVESCORE='$temp_above', BELOWSCORE='$temp_below', WINNER='-1' WHERE USERNO='$account' AND GAMENO='$gameno' AND PLAYNO='$i'");
 	}
+
 	if ($gametype == 'A' || $i > $gap) {
 		updateGameChart($account, $gameno);
 	}
